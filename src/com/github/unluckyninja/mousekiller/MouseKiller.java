88b264a616cb269f8ef8fc6619cc9e97c691d181
OpenShift guest <unluckyninja1994@gmail.com>
2013-07-21 17:47:58 -0400

    Building the main frame

--- src/com/github/unluckyninja/mousekiller/MouseKiller.java
+++ src/com/github/unluckyninja/mousekiller/MouseKiller.java
@@ -1,3 +1,11 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Drop extends Game {
+
 public class MouseKiller{
     public static void main(String[] args){
         
