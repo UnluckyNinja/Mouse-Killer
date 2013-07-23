f95b1a9661c90f31df80b61036beb452e13963d4
UnluckyNinja <unluckyninja1994@gmail.com>
2013-07-23 09:00:54 +0800

    adding the 6 funs.

--- src/com/github/unluckyninja/mousekiller/MouseKiller.java
+++ src/com/github/unluckyninja/mousekiller/MouseKiller.java
@@ -1,13 +1,41 @@
-package com.badlogic.drop;
+package com.github.unluckyninja.mousekiller;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Drop extends Game {
-
-public class MouseKiller{
+public class MouseKiller extends Game {
     public static void main(String[] args){
         
+    }
+
+    @Override
+    public void create() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void render() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void pause() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void resume() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void dispose() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
